7b334d24818f2a7578cff0f6065edc2db9a5bad5
Jamil-coder360 <jamil595nhj@gmail.com>
2026-06-16 19:03:29 +0600

    categories & categoriesCard add

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -3,18 +3,20 @@ import Section from '../global/Section';
 import Container from '../global/Container';
 import Button from '../global/Button';
 import { ArrowRight } from 'lucide-react';
+import CategoriesCard from './CategoriesCard';
 
 const Categories = () => {
   return (
    <Section>
     <Container>
         <div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between mb-8">
                 <h3 className="font-semibold text-[32px] leading-[120%] text-gray_900">
                     Popular Categories
                 </h3>
                 <Button variant={"transparent"} >View All <ArrowRight /> </Button>
             </div>
+            <CategoriesCard />
         </div>
     </Container>
    </Section>
--- src/components/categories/CategoriesCard.jsx
+++ src/components/categories/CategoriesCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import ctg from "../../assets/ctg.png"
 const CategoriesCard = () => {
   return (
-    <div>CategoriesCard</div>
+<div className="group flex flex-col items-center px-2 pt-4 border border-gray_100 w-[200px] rounded-[5px] hover:border-success_dark hover:shadow-xl hover:shadow-success_dark transition-all duration-300">
+        <div className="w-[190px] h-[130px] overflow-hidden">
+            <img src={ctg} alt=""  className="w-full h-full " />
+
+        </div>
+        <p className="font-medium text-[18px] leading-[150%] text-gray_900 pt-4 pb-6 group-hover:text-success_dark">Fresh Fruit</p>
+    </div>
   )
 }
 
