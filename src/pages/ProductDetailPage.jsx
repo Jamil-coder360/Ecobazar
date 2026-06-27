49f5caa2ffcebb53e784305bf0ffc7c9107f2eae
Jamil-coder360 <jamil595nhj@gmail.com>
2026-06-28 01:18:19 +0600

    Configure json-server for Render

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -22,7 +22,8 @@ const ProductDetailPage = () => {
 
   useEffect(() => {
     if (!id) return;
-    fetch(`http://localhost:3000/products/${id}`)
+    // fetch(`http://localhost:3000/products/${id}`)
+    fetch(`https://ecobazar-ktbd.onrender.com/products${id}`)
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, [id]);
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -11,7 +11,7 @@ const ShopPage = () => {
   const [sort, setSort] = useState("Latest");
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/products")
+    fetch("https://ecobazar-ktbd.onrender.com/products")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
